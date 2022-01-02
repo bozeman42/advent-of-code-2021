389fb7ecbd796767d0a81297cb1df0dbf4769e38
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2022-01-02 17:21:26 -0600

    explosions implemented

--- day18/d18-2.js
+++ day18/d18-2.js
@@ -5,16 +5,112 @@ const USE_EXAMPLE = true
 
 const FILENAME = USE_EXAMPLE ? 'example' : 'input'
 
-const EXPLODE_DEPTH = 5
+const EXPLODE_DEPTH = 4
 
 class Tree {
   constructor(array) {
-    this.tree = new TreeNode('', array)
+    this.tree = new TreeNode(array)
     this.getArray()
+    this.test()
   }
 
   add(tree) {
-    return new Tree([this.tree.getArray(), tree.getArray()])
+    this.tree = new TreeNode([this.tree, tree.tree])
+    this.test()
+    return this
+  }
+
+  test() {
+    let actionOccured = false
+    actionOccured = this.testForExplosion()
+    if (actionOccured) return this.test()
+    actionOccured = this.testForSplit()
+    if (actionOccured) return this.test()
+  }
+  
+  testForSplit() {
+    // implement test for split
+  }
+
+  testForExplosion() {
+    let stack = []
+    let explosion = false
+    let current = {
+      path: '',
+      tree: this.tree
+    }
+    stack.push(current)
+    do {
+      if (current.tree.leaf && stack.length) current = stack.pop()
+      while(!current.tree.leaf) {
+        current = {
+          path: current.path + '0',
+          tree: current.tree.left
+        }
+        if (!current.tree.leaf) {
+          stack.push(current)
+          continue
+        }
+      }
+      if (current.path.length > EXPLODE_DEPTH) {
+        current = stack.pop()
+        const { tree: {left: { value: left }, right: {value: right}}, path } = current
+        const parentPosition = path[path.length - 1]
+        this.prune(path)
+        current = stack.pop()
+        if (parentPosition === '0') {
+          current.tree.right.addLeft(right)
+        } else {
+          current.tree.left.addRight(left)
+        }
+        while (stack.length && current.path[current.path.length - 1] === parentPosition) {
+          current = stack.pop()
+        }
+        if (stack.length) {
+          current = stack.pop()
+          if (parentPosition === '0') {
+            current.tree.left.addRight(left)
+          } else {
+            current.tree.right.addLeft(right)
+          }
+        }
+        console.log(JSON.stringify(this.getArray()))
+        return true
+      } else {
+        current = stack.pop()
+        stack.push(current)
+        current = {
+          path: current.path + '1',
+          tree: current.tree.right
+        }
+        if (!current.tree.leaf) stack.push(current)
+      }
+      if (this.getLastPathItem(current) === '1' && current.tree.leaf) {
+        while(current.path[current.path.length - 1] === '1') {
+          current = stack.pop()
+        }
+      }
+    } while (stack.length)
+    return false
+  }
+
+  getLastPathItem(item) {
+    return item.path[item.path.length - 1]
+  }
+
+  prune(path) {
+    let current = this.tree
+    let i
+    for(i = 0; i < path.length - 1; i++) {
+      if (path[i] === '0') {
+        current = current.left
+      } else {
+        current = current.right
+      }
+    }
+    path[i] === '0'
+      ? current.left = new TreeNode(0)
+      : current.right = new TreeNode(0)
   }
 
   getArray() {
@@ -23,19 +119,30 @@ class Tree {
 }
 
 class TreeNode {
-  constructor(path, value) {
-    this.path = path
+  constructor(value) {
+    this.leaf = false
     if (Array.isArray(value)) {
-      this.left = new TreeNode(path + 0, value[0])
-      this.right = new TreeNode(path + 1, value[1])
+      this.left = value[0].constructor.name === 'TreeNode' ? value[0] : new TreeNode(value[0])
+      this.right = value[1].constructor.name === 'TreeNode' ? value[1] : new TreeNode(value[1])
       this.value = null
-      this.leaf = false
     } else {
       this.left = null
       this.right = null
       this.value = value
-      this.leaf = true
+      if (typeof value === 'number') {
+        this.leaf = true
+      }
     }
+  }
+
+  addLeft(n) {
+    if (this.leaf) return this.value += n
+    this.left.addLeft(n)
+  }
+
+  addRight(n) {
+    if (this.leaf) return this.value += n
+    this.right.addRight(n)
   }
 
   getArray() {
@@ -48,10 +155,8 @@ const input = readFile(resolve(__dirname, `${FILENAME}.txt`))
   .split('\n')
   .map(line => new Tree(JSON.parse(line)))
 
-console.log(input)
-
 let result = input.reduce((result, pair) => {
   return result.add(pair)
 })
 
-console.log(result.tree)
+console.log(JSON.stringify(result.getArray()))
